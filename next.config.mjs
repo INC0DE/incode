28400c3e7856b65f395a6f970a8ffa16bd0626a2
Carlos Pacheco Aviles <carlospachecoaviles@MacBook-Pro-de-Carlos.local>
2025-03-17 23:42:18 -0600

    js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 export default {
-  output: "export", 
+  output: "export",
   distDir: "out", // Carpeta de salida
   images: {
     unoptimized: true, // Deshabilita la optimización de imágenes (necesario para GitHub Pages)
   },
   basePath: "/incode", // Reemplaza con el nombre de tu repositorio
   assetPrefix: "/incode/", // Ajusta las rutas de los assets
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+    };
+  },
 };
