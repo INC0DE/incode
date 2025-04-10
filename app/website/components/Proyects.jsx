4f87e26c0c190b75e662474350844b708f9a2335
incodewebm <incodewebm@gmail.com>
2025-04-10 16:06:04 -0600

    Proyects

--- app/website/components/Proyects.jsx
+++ app/website/components/Proyects.jsx
@@ -1,34 +1,29 @@
 "use client";
 
-import Image from "next/image";
+import Link from "next/link";
 import TiltedCard from "./TiltedCard";
 
 const images = [
   {
-    src: "/assets/corporativas.jpg",
-    title: "Landing-Pages",
+    src: "/assets/evolutiongym.jpeg",
+    title: "Evolution GYM",
+    href: "https://evolution-swart.vercel.app/",
   },
   {
-    src: "/assets/e-commerce.jpeg",
-    title: "E-Commerce",
+    src: "/assets/josecarlos.jpeg",
+    title: "Jose Carlos",
+    href: "https://www.josecarloscervantes.com/",
   },
   {
-    src: "/assets/blog.jpg",
-    title: "Blog",
-  },
-  {
-    src: "/assets/menu.jpg",
-    title: "Menú",
-  },
-  {
-    src: "/assets/portfolio.jpg",
-    title: "Portafolio",
+    src: "/assets/incode.jpeg",
+    title: "INCODE",
+    href: "https://incodeweb.com/",
   },
 ];
 
 const Proyects = () => {
   return (
-    <section className="w-full min-h-screen flex flex-col bg-whitelight items-center gap-20">
+    <section className="w-full lg:min-h-screen flex flex-col bg-whitelight items-center gap-20 py-5">
       <div className="bg-white w-full h-56 content-center">
         <h1 className="text-5xl text-center lg:text-7xl text-gray">
           Nuestros{" "}
@@ -36,23 +31,29 @@ const Proyects = () => {
         </h1>
       </div>
 
-      <div className="relative w-full flex flex-col gap-10 md:flex-row md:gap-20 md:overflow-x-auto scrollbar-hide md:p-20">
+      <div className="relative w-full flex flex-col gap-10  md:flex-row md:gap-20 md:overflow-x-auto scrollbar-hide md:p-20">
         {images.map((item, index) => (
-          <TiltedCard
-            key={index}
-            imageSrc={item.src}
-            altText={item.title}
-            captionText={item.title}
-            imageWidth="350px"
-            rotateAmplitude={12}
-            scaleOnHover={1.2}
-            showMobileWarning={false}
-            showTooltip={true}
-            displayOverlayContent={true}
-            overlayContent={
-              <p className="tilted-card-demo-text text-gold">{item.title}</p>
-            }
-          />
+          <a
+            href={item.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TiltedCard
+              key={index}
+              imageSrc={item.src}
+              altText={item.title}
+              captionText={item.title}
+              imageWidth="550px"
+              rotateAmplitude={12}
+              scaleOnHover={1.2}
+              showMobileWarning={false}
+              showTooltip={true}
+              displayOverlayContent={true}
+              overlayContent={
+                <p className="tilted-card-demo-text text-gold">{item.title}</p>
+              }
+            />
+          </a>
         ))}
       </div>
     </section>
